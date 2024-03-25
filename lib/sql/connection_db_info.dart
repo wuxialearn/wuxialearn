289e8a85b2f96addf2e6c66ccc0962975aaf4841
wuxialearn <160411494+wuxialearn@users.noreply.github.com>
2024-03-25 14:42:22 -0400

    add conninfo.dart

---
+++ lib/sql/connection_db_info.dart
@@ -0,0 +1,8 @@
+final class ConnectionInfo{
+  final String host = "";
+  final int port = 0;
+  final String databaseName = "";
+  final String username = "";
+  final password = "";
+  final bool useSSL = true;
+}
